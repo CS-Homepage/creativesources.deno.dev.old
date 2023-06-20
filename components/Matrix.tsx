74c710dec9d4f402efc4f632dfa0da245cff2e4b
Spoof My ARP <spoofmyarp@proton.me>
2023-06-20 22:53:43 +0200

    Test Yandeu

---
+++ components/Matrix.tsx
@@ -0,0 +1,34 @@
+import { h, Fragment } from "../deps.ts";
+
+export default function Matrix() {
+  console.log("Matrix started...");
+
+  const bg = document.getElementById("bg");
+
+  const div = document.createElement("div");
+  div.id = "matrix";
+    
+  let n = 0;
+  let value: number;
+ 
+  while (n < 80) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    column.classList.add(`col-${++n}`);
+    div.appendChild(column);
+    let i = 0;
+    while (i < 50) {
+      console.log("GLYPH");
+      ++i;
+      value = Math.floor(Math.random() * 255) + 10240;
+      const span = document.createElement("span");
+      const glyph = document.createTextNode(`&#${value};`);
+      span.appendChild(glyph);
+      column.appendChild(span);
+    }
+  }
+
+  console.log(div);
+  if (bg) bg.appendChild(div); // Why doesn't this work ???
+  console.log(bg);
+}
